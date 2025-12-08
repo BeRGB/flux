6e0143eb02724e1a1cc110a28fd6f1e77a49011b
Nikola Arezina <arezina@gmail.com>
2025-12-08 15:26:22 +0100

    Add GitHub Pages deployment workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+	site: process.env.SITE_URL ?? 'http://localhost:4321',
+	base: process.env.BASE_PATH ?? '/',
 	integrations: [react()],
 });
